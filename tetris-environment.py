c155370a2b420f627a26dacaa2617d64f3679d3f
womyat <jannes.doege@manos-dresden.lernsax.de>
2021-07-05 13:53:44 +0200

    Update tetris-environment.py

--- tetris-environment.py
+++ tetris-environment.py
@@ -21,23 +21,39 @@ class Shape:
             self.rot = 270
         self.shape = self.shapes["rot" + str(self.rot)]
 
+lblock = Shape()
+jblock = Shape()
+sblock = Shape()
+zblock = Shape()
+tblock = Shape()
+iblock = Shape()
+sqrblock = Shape()
 
 class Tetris(gym.Env):
     def __init__(self):
         self.board = np.zeros((10, 40))
-
+        self.blocks = [lblock, jblock, sblock, zblock, tblock, iblock, sqrblock]
+        self.blocks_buf = [i for i in self.blocks]
+        self.blocks_buf.shuffle()
+        self.step_idx = 0
+        self.free_fall = False
+        self.current_block = None
+        
     def step(self, action):
-        pass
+        self.step_idx += 1
+        
+        if self.step_idx % len(self.blocks) == 0:
+            self.blocks = [lblock, jblock, sblock, zblock, tblock, iblock, sqrblock]
+            self.blocks_buf = [i for i in self.blocks]
+        
+        if not self.free_fall:
+            self.current_block = self.blocks_buf.pop(0)
+
+
 
     def reset(self):
         pass
 
     def render(self, render_mode="human"):
         pass
-
-s = np.zeros((3, 3))
-n = np.zeros((1, 1))
-p = np.zeros((2, 2))
-d = np.zeros((4, 4))
-
-shape = Shape(s, n, p, d)
+    
