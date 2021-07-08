3079b0fe33b436268736e43c6ce8ea1cfed33604
womyat <jannes.doege@manos-dresden.lernsax.de>
2021-07-08 21:06:43 +0200

    Update tetris-environment.py

--- tetris-environment.py
+++ tetris-environment.py
@@ -1,27 +1,8 @@
 import gym
 import numpy as np
 
-class Shape:
-    def __init__(self, rot0, rot90, rot180, rot270):
-        self.rot = 0
-        self.shapes = {
-            "rot0": rot0,
-            "rot90": rot90,
-            "rot180": rot180,
-            "rot270": rot270
-        }
 
-        self.shape = self.shapes["rot0"]
-    
-    def rotate(self, direct:int):
-        self.rot += direct * 90
-        if self.rot > 270:
-            self.rot = 0
-        if self.rot < 0:
-            self.rot = 270
-        self.shape = self.shapes["rot" + str(self.rot)]
 
-lblock = Shape([(1, 1), (1, 0), (0, 1), (1, 0)], [(1, 1), (1, 0), (0, 1), (1, 0)], [(1, 1), (1, 0), (0, 1), (1, 0)], [(1, 1), (1, 0), (0, 1), (1, 0)])
 #jblock = Shape()
 #sblock = Shape()
 #zblock = Shape()
@@ -30,10 +11,20 @@ lblock = Shape([(1, 1), (1, 0), (0, 1), (1, 0)], [(1, 1), (1, 0), (0, 1), (1, 0)
 #sqrblock = Shape()
 
 class Tetris(gym.Env):
+    TETRAMINOS = [
+        [(1, 1), (1, 0), (0, 1), (1, 0)], # lblock
+        #jblock 
+        #sblock 
+        #zblock 
+        #tblock 
+        #iblock 
+        #sqrblock
+
+    ]
+
     def __init__(self):
         self.board = np.zeros((10, 40))
-        self.blocks = [lblock, jblock, sblock, zblock, tblock, iblock, sqrblock]
-        self.blocks_buf = [i for i in self.blocks]
+        self.blocks_buf = [i for i in Tetris.TETRAMINOS]
         self.blocks_buf.shuffle()
         self.step_idx = 0
         self.current_block = None
@@ -43,14 +34,24 @@ class Tetris(gym.Env):
         self.step_idx += 1
         
         if self.step_idx % len(self.blocks) == 0:
-            self.blocks = [lblock, jblock, sblock, zblock, tblock, iblock, sqrblock]
+            self.blocks_buf = [i for i in Tetris.TETRAMINOS]
             self.blocks_buf = [i for i in self.blocks]
         
         if not self.free_fall:
+            self.block_count += 1
             self.current_block = self.blocks_buf.pop(0)
         else:
-            pass
+            for i in range(0, self.block_count -1 ):
+                fall = False
+                for j in self.board:
+                    if j == i && # check below
         
+        # if line clear
+        for i in range(0, self.block_count -1 ):
+                fall = False
+                for j in self.board:
+                    if j == i && # check below
+
         # every block has its own number
         # this is used to get sticky line clears
 
